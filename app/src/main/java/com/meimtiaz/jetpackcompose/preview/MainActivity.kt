c77b85baea995c14469e0eacfcaea5e5eca87f57
imtiazovi020 <imtiazuddin018@gmail.com>
2022-06-08 01:38:49 +0600

    Show text and preview

--- app/src/main/java/com/meimtiaz/jetpackcompose/preview/MainActivity.kt
+++ app/src/main/java/com/meimtiaz/jetpackcompose/preview/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.meimtiaz.jetpackcompose
+package com.meimtiaz.jetpackcompose.preview
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,29 +17,24 @@ import com.meimtiaz.jetpackcompose.ui.theme.JetpackcomposeplaygroundTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetpackcomposeplaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        
+        setContent { 
+            Greeting(name = "Imtiaz")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackcomposeplaygroundTheme {
-        Greeting("Android")
+        Greeting("Md Imtiaz Uddin")
     }
 }
